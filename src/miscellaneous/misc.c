b2bb4cfa31d7c34170b99593fd060e87bd5f7db5
Vlad Kolodiy <vladislavkolodiy45@gmail.com>
2026-02-03 16:38:56 +0200

    mpg123 works on windows!

--- src/miscellaneous/misc.c
+++ src/miscellaneous/misc.c
@@ -1,3 +1,9 @@
+#include "misc.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <stdlib.h>
+
 int clamp_int(int value, int min_val, int max_val) {
     if (value < min_val) {
         return min_val;
@@ -17,4 +23,18 @@ float clamp_float(float value, float min_val, float max_val) {
         return value;
     }
 }
+
+#ifdef _WIN32
+char* wchar_to_char_alloc(const wchar_t* wstr) {
+    if (!wstr) return NULL;
+
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+    char* strTo = (char*)malloc(size_needed);
+    if (!strTo) return NULL; 
+
+    WideCharToMultiByte(CP_UTF8, 0, wstr, -1, strTo, size_needed, NULL, NULL);
+    
+    return strTo;
+}
+#endif
 
--- src/miscellaneous/misc.h
+++ src/miscellaneous/misc.h
@@ -1,7 +1,12 @@
 #ifndef MISC_H
 #define MISC_H
 
+#include <stddef.h> // <--- Add this to define wchar_t
+#include <stdlib.h> // Good to have here for size_t
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,6 +14,10 @@ extern "C" {
 
 int clamp_int(int value, int min_val, int max_val);
 float clamp_float(float value, float min_val, float max_val);
+
+#ifdef _WIN32
+char* wchar_to_char_alloc(const wchar_t* wstr);
+#endif
 
 #ifdef __cplusplus
 }
